86d9b207f98d5d48de945423ee8f426cfe124171
Didier <didorg@gmail.com>
2020-03-23 14:59:50 -0400

    first commit

---
+++ src/main/java/com/elensol/storiesconfigserver/GithubSpringCloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.elensol.storiesconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubSpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubSpringCloudConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elensol/storiesconfigserver/GithubSpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elensol.storiesconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubSpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
