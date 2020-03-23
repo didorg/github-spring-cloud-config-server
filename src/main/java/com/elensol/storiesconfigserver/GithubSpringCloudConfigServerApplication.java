abf224884833e9331f9e3efe73f8f1b9ce0f8feb
Didier <didorg@gmail.com>
2020-03-23 15:29:41 -0400

    Update GithubSpringCloudConfigServerApplication.java

--- src/main/java/com/elensol/storiesconfigserver/GithubSpringCloudConfigServerApplication.java
+++ src/main/java/com/elensol/storiesconfigserver/GithubSpringCloudConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.elensol.storiesconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class GithubSpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
